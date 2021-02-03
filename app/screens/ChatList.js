a481c6e096a545deb9d482d4c14b442cdf6bb635
yoonz <yoonz44@naver.com>
2021-02-04 02:44:26 +0900

    date in

--- app/screens/ChatList.js
+++ app/screens/ChatList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, FlatList, SafeAreaView, Text, TouchableHighlight, View, StyleSheet} from "react-native";
 
 const data = Array(30).fill(1).map((value, index) => {
@@ -9,28 +9,47 @@ const data = Array(30).fill(1).map((value, index) => {
 });
 
 export default function ChatList({navigation}) {
+    const [channelList, setChannelList] = useState([]);
+
+    useEffect(() => {
+        fetch('https://back.talkpic.kr/api/channel/today', {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => response.json())
+            .then(responseJson => {
+                setChannelList(responseJson);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
     return (
         <SafeAreaView style={styles.container}>
             <FlatList
                 ItemSeparatorComponent={Platform.OS !== 'android' && (highlighted => (
                     <View style={[styles.separator, highlighted && {marginLeft: 0}]}/>
                 ))}
-                data={data}
+                data={channelList}
                 renderItem={({item, index, separators}) => (
                     <TouchableHighlight
-                        key={item.key}
+                        key={item.id}
                         onPress={() => navigation.navigate('Chat', {
-                            roomId: item.key
+                            roomId: item.id
                         })}
                         onShowUnderlay={separators.highlight}
                         onHideUnderlay={separators.unhighlight}
                     >
                         <View style={{backgroundColor: 'white'}}>
-                            <Text>{item.title}</Text>
+                            <Text>{`${item.broad_caster}-${item.category}`}</Text>
                             <Button
-                                title={item.title}
+                                title={item.program_name}
                                 onPress={() => navigation.navigate('Chat', {
-                                    roomId: item.key
+                                    roomId: item.id
                                 })}
                             />
                         </View>
